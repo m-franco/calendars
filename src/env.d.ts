91bb5ce2b36a315a240e464f82186b5f9032e789
Matias Franco <matiasfranco86@gmail.com>
2024-04-16 05:33:09 -0300

    import meta

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="astro/client" />
+interface ImportMetaEnv {
+    readonly PUBLIC_IMAGE_PATH: string;
+    // more env variables...
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
