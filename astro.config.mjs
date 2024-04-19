815f3e5ca2966b7aca944bb259ad00d66d9c2246
Matias Franco <matiasfranco86@gmail.com>
2024-04-19 08:30:00 -0300

    routing fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://m-franco.github.io',
-  base: 'calendars',
   integrations: [tailwind()]
 });
