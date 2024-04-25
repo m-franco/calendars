f6fb14614a83e91afc063d092ff182d1e0835bd1
Matias Franco <matiasfranco86@gmail.com>
2024-04-25 10:46:08 -0300

    partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,18 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
+import partytown from '@astrojs/partytown'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://m-franco.github.io',
-  base: 'calendars',
-  integrations: [tailwind()]
+    site: 'https://m-franco.github.io',
+    base: 'calendars',
+    integrations: [
+        tailwind(),
+        partytown({
+            config: {
+                forward: ["dataLayer.push"],
+            },
+        }),
+    ]
 });
